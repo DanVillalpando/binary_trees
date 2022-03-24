3ac832f27af1eb36567072cfedb282c0d46a18b5
DanVillalpando <danielvch5@gmail.com>
2022-03-24 11:21:45 -0700

    fix 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,8 +7,8 @@
  * Return: new node, or NULL on failure
  */binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
-	new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
 	if (new == NULL)
 		return (NULL);
 
