2f9a2d6db5a8a19d525265fdf6b06f8372d839ff
DanVillalpando <danielvch5@gmail.com>
2022-03-24 20:55:24 -0700

    fix 7 & 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,13 +7,13 @@
  * Return: new node, or NULL on failure
  */void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
+
 	if (tree != NULL)
 	{
 		binary_tree_inorder(tree->left, func);
 		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
-
-	if (tree == NULL || func == NULL)
-		return;
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,13 +7,13 @@
  * Return: new node, or NULL on failure
  */void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
+
 	if (tree != NULL)
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
-
-	if (tree == NULL || func == NULL)
-		return;
 }
