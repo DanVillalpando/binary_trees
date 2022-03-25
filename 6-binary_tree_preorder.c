fdaac5f2c65fe0fc5485f1120a722defd2cc5e46
DanVillalpando <danielvch5@gmail.com>
2022-03-24 20:46:03 -0700

    fix 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,13 +7,13 @@
  * Return: new node, or NULL on failure
  */void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
+
 	if (tree != NULL)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
-
-	if (tree == NULL || func == NULL)
-		return;
 }
