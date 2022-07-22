7029e53f6b5a314fb94acdde95680cfb89d0e3e0
DanVillalpando <danielvch5@gmail.com>
2022-07-21 21:45:01 -0700

    9 fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,3 +18,18 @@
 		return (height_L + 1);
 	return (height_R + 1);
 }
+#include "binary_trees.h"
+
+/***
+ * Update
+ * Returns 1 if node is a leaf
+ * 0 if not
+ ***/int binary_tree_is_leaf(const binary_tree_t *node)
+{
+        if (node == NULL)
+                return (0);
+
+        if (node->right == NULL && node->left == NULL)
+                return (1);
+        return (0);
+}
